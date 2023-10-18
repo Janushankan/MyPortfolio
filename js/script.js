d8b60a9012f5ce86726b2c72925ba8733aa0e51e
V. Janushankan <vj.janushankan1006@gmail.com>
2023-10-19 00:57:36 +0530

    skills added

--- js/script.js
+++ js/script.js
@@ -41,14 +41,14 @@ window.onscroll = () => {
 ScrollReveal({
     // reset: true,
     distance: '80px',
-    duration:2000,
+    duration: 2000,
     delay: 200
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form, .skill-item', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content, .skill-img', { origin: 'right' });
 
 /* typed js */
 const typed = new Typed('.multiple-text', {
