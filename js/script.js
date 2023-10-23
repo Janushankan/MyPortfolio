16a2264e6976beed449befb35da7e5faaf1dfd9e
V. Janushankan <vj.janushankan1006@gmail.com>
2023-10-24 00:40:55 +0530

    education added

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form, .skill-item', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h1, .about-img, .education-box, .title', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content, .skill-img', { origin: 'right' });
 
 /* typed js */
